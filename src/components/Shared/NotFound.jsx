6f79fba73e5959e5c1dd93bc965da8ff3e853ca0
Ahmad Subaktagin Jabir <subaktagin.jabir16@gmail.com>
2024-07-04 08:41:38 +0600

    Error Page Modified and Toggle Design for Package Modified

--- src/components/Shared/NotFound.jsx
+++ src/components/Shared/NotFound.jsx
@@ -1,13 +1,25 @@
+import { Link } from "react-router-dom";
 import notFound from "../../assets/error.png";
 
 export const NotFound = () => {
   return (
-    <div className="my-10 ">
-      <div className="flex justify-center items-center">
-        <div className="lg:w-6/12">
+    <div className="grid h-screen place-content-center px-4">
+      <div className="text-center">
+        <div className="lg:w-4/12 mx-auto">
           <img src={notFound} alt="Not Found Error" />
         </div>
+        <h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          Uh-oh!
+        </h1>
+
+        <p className="mt-4 text-gray-500">We can't find that page.</p>
       </div>
+      <Link
+        to={"/"}
+        className="text-center mt-6 inline-block rounded-none bg-primary px-5 py-3 text-sm font-medium text-white hover:bg-opacity-85 focus:outline-none focus:ring"
+      >
+        Go Back Home
+      </Link>
     </div>
   );
 };
--- src/pages/Vendor/Packages/AddNewPackage.jsx
+++ src/pages/Vendor/Packages/AddNewPackage.jsx
@@ -91,12 +91,49 @@ export const AddNewPackage = () => {
                   <div className="form-control">
                     <label className="cursor-pointer label">
                       <span className="label-text">Discount</span>
-                      <input
-                        type="checkbox"
-                        className="toggle toggle-primary"
-                        checked={discountChecked}
-                        onChange={handleDiscountCheck}
-                      />
+
+                      <label
+                        htmlFor="AcceptConditions"
+                        className="relative inline-block h-8 w-14 cursor-pointer rounded-full bg-gray-300 transition [-webkit-tap-highlight-color:_transparent] has-[:checked]:bg-primary"
+                      >
+                        <input
+                          type="checkbox"
+                          id="AcceptConditions"
+                          className="peer sr-only [&:checked_+_span_svg[data-checked-icon]]:block [&:checked_+_span_svg[data-unchecked-icon]]:hidden"
+                          checked={discountChecked}
+                          onChange={handleDiscountCheck}
+                        />
+
+                        <span className="absolute inset-y-0 start-0 z-10 m-1 inline-flex size-6 items-center justify-center rounded-full bg-white text-gray-400 transition-all peer-checked:start-6 peer-checked:text-accent">
+                          <svg
+                            data-unchecked-icon
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+
+                          <svg
+                            data-checked-icon
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="hidden h-4 w-4"
+                            viewBox="0 0 20 20"
+                            fill="currentColor"
+                          >
+                            <path
+                              fillRule="evenodd"
+                              d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
+                              clipRule="evenodd"
+                            />
+                          </svg>
+                        </span>
+                      </label>
                     </label>
                   </div>
                   {discountChecked && (
@@ -110,7 +147,6 @@ export const AddNewPackage = () => {
                         type="number"
                         placeholder="Enter Discount Percentage"
                         value={discountValue}
-                        onChange={(e) => setDiscountValue(e.target.value)}
                         max={100}
                         {...register("discountPercentage")}
                       />
